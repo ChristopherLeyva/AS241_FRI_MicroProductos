53d6fe7933ca98e7cad4ad320b4df006f99cd0f6
ChristopherLeyva <christopher.leyva.chumpitaz@vallegrande.edu.pe>
2026-06-28 21:12:54 -0500

    fix: add @Repository to adapter, remove from port interface

--- src/main/java/pe/edu/vallegrande/msproductos/application/port/out/IProductoRepositoryPort.java
+++ src/main/java/pe/edu/vallegrande/msproductos/application/port/out/IProductoRepositoryPort.java
@@ -1,11 +1,9 @@
 package pe.edu.vallegrande.msproductos.application.port.out;
 
-import org.springframework.stereotype.Repository;
 import pe.edu.vallegrande.msproductos.domain.model.Producto;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@Repository
 public interface IProductoRepositoryPort {
 
     Flux<Producto> findAllActive();
--- src/main/java/pe/edu/vallegrande/msproductos/infrastructure/adapter/out/persistence/ProductoRepositoryAdapter.java
+++ src/main/java/pe/edu/vallegrande/msproductos/infrastructure/adapter/out/persistence/ProductoRepositoryAdapter.java
@@ -1,11 +1,13 @@
 package pe.edu.vallegrande.msproductos.infrastructure.adapter.out.persistence;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 import pe.edu.vallegrande.msproductos.application.port.out.IProductoRepositoryPort;
 import pe.edu.vallegrande.msproductos.domain.model.Producto;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Repository
 @RequiredArgsConstructor
 public class ProductoRepositoryAdapter implements IProductoRepositoryPort {
 
