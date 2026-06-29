f621fcea2dada85db1d5eaec681c333eaf61cc9e
ChristopherLeyva <christopher.leyva.chumpitaz@vallegrande.edu.pe>
2026-06-28 19:46:48 -0500

    fix: update Producto model fields (name, price, stock, active)

--- src/main/java/pe/edu/vallegrande/msproductos/domain/model/Producto.java
+++ src/main/java/pe/edu/vallegrande/msproductos/domain/model/Producto.java
@@ -1,67 +1,21 @@
 package pe.edu.vallegrande.msproductos.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table("productos")
 public class Producto {
 
     @Id
     private Long id;
-    private String codigo;
-    private String nombre;
-    private Double precio;
+    private String name;
+    private Double price;
     private Integer stock;
-
-    public Producto() {
-    }
-
-    public Producto(Long id, String codigo, String nombre, Double precio, Integer stock) {
-        this.id = id;
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(Double precio) {
-        this.precio = precio;
-    }
-
-    public Integer getStock() {
-        return stock;
-    }
-
-    public void setStock(Integer stock) {
-        this.stock = stock;
-    }
-
+    private Boolean active;
 }
